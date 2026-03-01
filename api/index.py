25a693a21134d42fc4ea85c7672a5401c41e6588
kadivarsaman <kadivarsaman@gmail.com>
2026-03-01 10:12:03 +0530

    index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Cardio ML API is running successfully 🚀"
-    }
+def handler(request, response):
+    response.status_code = 200
+    response.body = "CardioVascular API is live 🚀"
+    return response
